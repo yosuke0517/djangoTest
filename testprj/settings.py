79c8b49a0290353be326124a9176ba8e29ee86ca
yosuke0517 <x.clover.x007@gmail.com>
2021-01-15 00:44:22 +0900

    init

--- testprj/settings.py
+++ testprj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'testprj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lotterydb',
+        'USER': 'lottery',
+        'PASSWORD': 'lottery',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
